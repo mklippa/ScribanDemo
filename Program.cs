878f50d83b238a7655a0c4caebfa87f7b004b73f
Maxim Klippa <maxim.klippa@arcadia.spb.ru>
2018-09-28 00:09:52 +0300

    add entities

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Scriban;
 
 namespace ScribanDemo
 {
@@ -6,7 +9,36 @@ namespace ScribanDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var template = Template.Parse(File.ReadAllText("input.html"));
+            var result = template.Render(new { Name = "World" }); // => "Hello World!" 
+            File.WriteAllText("output.html", result);
         }
+    }
+
+    class TestResult
+    {
+        public IEnumerable<Section> Sections { get; set; }
+    }
+
+    class Section
+    {
+        public IEnumerable<ItemResponse> Items { get; set; }
+    }
+
+    public class ItemResponse
+    {
+                public Question Question { get; set; }
+    }
+
+    public class Question
+    {
+        public IEnumerable<QuestionMetadata> Metadatas { get; set; }
+    }
+
+    public class QuestionMetadata
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
     }
 }
