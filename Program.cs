83cca48fceeab7526ffa57d3ffc847cc70110444
Maxim Klippa <maxim.klippa@arcadia.spb.ru>
2018-09-27 23:55:16 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScribanDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
